f10fdc3624f4c4a17014ebf03c9b7cdd391dfc9f
Olivier-tlse3 <131273830+Olivier-tlse3@users.noreply.github.com>
2026-01-30 09:54:38 +0100

    fix #4.1 classe Partnership

---
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -0,0 +1,31 @@
+package ourbusinessproject;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+
+public class Partnership {
+    @NotNull
+    private Date date;
+
+    @NotNull
+    private Enterprise enterprise;
+    @NotNull
+    private Project project;
+
+    public Partnership(){}
+
+    public void setCreationDate(Date date) {
+        this.date = date;
+    }
+
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
