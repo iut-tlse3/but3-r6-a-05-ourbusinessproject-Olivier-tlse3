d91f625623e6bafb28421d1de6d1952ba2f8dc61
Olivier-tlse3 <131273830+Olivier-tlse3@users.noreply.github.com>
2026-01-30 10:57:54 +0100

    fix #4.2 classe PartnershipService

--- src/main/java/ourbusinessproject/Partnership.java
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -1,17 +1,23 @@
 package ourbusinessproject;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
-
+@Entity
 public class Partnership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotNull
     private Date date;
 
-    @NotNull
+    @NotNull @ManyToOne
     private Enterprise enterprise;
-    @NotNull
+    @NotNull @OneToOne
     private Project project;
 
     public Partnership(){}
@@ -27,5 +33,21 @@ public class Partnership {
 
     public void setProject(Project project) {
         this.project = project;
+    }
+
+    public Object getId() {
+        return  this.id;
+    }
+
+    public Enterprise getEnterprise() {
+        return this.enterprise;
+    }
+
+    public Date getCreationDate() {
+        return this.date;
+    }
+
+    public Project getProject() {
+        return this.project;
     }
 }
